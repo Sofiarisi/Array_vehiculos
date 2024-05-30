512719bbe21be42c33f3a3267790f2fd493b5408
Sofiarisi <48591347@est.ort.edu.ar>
2024-05-30 15:51:22 -0300

    Array ejercicios

---
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlquilerVehiculos : MonoBehaviour
+{
+    public GameObject[] vehiculos;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetearVehiculos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            DesactivarTodos();
+            int random = Random.Range(0, vehiculos.Length);
+            vehiculos[random].SetActive(true);
+        }
+
+        
+    }
+    void DesactivarTodos()
+    {
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
+    }
+
+    void ResetearVehiculos()
+    {
+        DesactivarTodos();
+        vehiculos[0].SetActive(true);
+
+    }
+}
